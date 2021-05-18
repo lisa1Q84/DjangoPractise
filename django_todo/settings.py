06636d72507fdcf3457d424182eb7cca4e5f858d
lisa1Q84 <lisafeigen@web.de>
2021-05-18 16:24:04 +0000

    correct development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -78,15 +78,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
 if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
